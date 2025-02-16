9dde18420151be3a95eb6f4a639d9d6b4c437553
codefarmer <junchengillusian@gmail.com>
2025-02-16 22:48:39 +0800

    add date range filter

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,4 +1,5 @@
 import React, { useEffect, useState } from 'react';
+
 import ItemList from './components/ItemList';
 import Pagination from './components/Pagination';
 
@@ -8,25 +9,27 @@ function App() {
   const [pageSize, setPageSize] = useState(10);
   const [search, setSearch] = useState('');
   const [total, setTotal] = useState(0);
+  const [startDate, setStartDate] = useState('');
+  const [endDate, setEndDate] = useState('');
 
-  useEffect(() => {
-    fetch(`http://localhost:5000/api/items?page=${page}&pageSize=${pageSize}&search=${search}`)
+  const fetchItems = () => {
+    fetch(`http://localhost:5000/api/items?page=${page}&pageSize=${pageSize}&search=${search}&startDate=${startDate}&endDate=${endDate}`)
       .then(res => res.json())
       .then(data => {
         setItems(data.items);
         setTotal(data.total);
       });
-  }, [page, pageSize, search]);
+  };
+
+  useEffect(() => {
+    fetchItems();
+  }, [page, pageSize, search, startDate, endDate])
 
   return (
     <div className='p-4'>
-      <input 
-        type='text' 
-        placeholder='Search by name' 
-        className='border p-2 mb-4' 
-        value={search} 
-        onChange={(e) => setSearch(e.target.value)} 
-      />
+      <input type='text' placeholder='Search by name' className='border p-2' value={search} onChange={(e) => setSearch(e.target.value)} />
+      <input type='date' onChange={(e) => setStartDate(e.target.value)} className='border p-2' placeholder='Start Date' />
+      <input type='date' onChange={(e) => setEndDate(e.target.value)} className='border p-2' placeholder='End Date' />
       <ItemList items={items} />
       <Pagination 
         page={page} 
